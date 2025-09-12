7a0ae97d141310fb07130252a3a2e35a6f0ee066
Gust Who? <lucgustga@outlook.com>
2025-09-13 03:55:27 +0800

    invite button

--- build/main.py
+++ build/main.py
@@ -2,16 +2,26 @@ import json
 import os
 import re
 
-script = ["// ==UserScript=="]
+metadata = ["// ==UserScript=="]
 
 with open("userscript.json") as f:
     config = json.load(f)
 
 for k,v in config.items():
-    script.append(f"// @{k}{" "*(15-len(k))}{v}")
+    metadata.append(f"// @{k}{" "*(15-len(k))}{v}")
 
-script.append("// ==/UserScript==")
+metadata.append("// ==/UserScript==")
+script = []
+script.append("\n".join(metadata))
+
 script.append("""(function() {'use strict';""")
+
+with open("src/style.css","r") as f:
+    script.append(f"""
+const style = document.createElement("style");
+style.innerHTML = `{f.read()}`
+document.body.prepend(style)
+""")
 
 def walk(file):
     with open(file,"r") as f:
@@ -22,9 +32,15 @@ def walk(file):
     return content
 
 script.append(walk("src/main.js"))
+
 script.append("""})();""")
 
-with open("dist/unformatted.js","w+") as f:
+with open("dist/RoWindy.dev.js","w+") as f:
     f.write("\n".join(script))
 
-os.system("js-beautify -r dist/RoWindy.userscript.js")
+os.system("js-beautify -r dist/RoWindy.dev.js")
+os.system("terser dist/RoWindy.dev.js --compress --mangle --output dist/RoWindy.js")
+
+with open("dist/RoWindy.userscript.js","w+") as f:
+    with open("dist/RoWindy.js","r") as r:
+        f.write("\n".join(["\n".join(metadata),"\n// Source at https://github.com/grhw/RoWindy\n",r.read()]))
--- src/additions/servers.js
+++ src/additions/servers.js
@@ -5,17 +5,39 @@ hookedPage.push((srv)=>{
 
     if (!snipe && joinButton && placeIdSpan) {
         const server_id = joinButton.getAttribute("data-btr-instance-id");
-        const place_id = joinButton.getAttribute("data-placeid");
+        const place_id = placeIdSpan.getAttribute("data-placeid");
         
         const container = document.createElement("div");
-        const force_join = document.createElement("button");
         container.className = "rw-srv";
 
+        const force_join = document.createElement("button");
         force_join.onclick = ()=>{
             window.Roblox.GameLauncher.joinGameInstance(place_id,server_id);
         }
         force_join.innerText = "Force Join";
+
+
+        const invite_link = document.createElement("button");
+        invite_link.onclick = ()=>{
+            navigator.clipboard.writeText(`https://www.roblox.com/games/${place_id}/#rw-invite=${server_id}`);
+            invite_link.innerText = "Copied!";
+            setTimeout(()=>{
+                invite_link.innerText = "Invite";
+            },500)
+        }
+        invite_link.innerText = "Invite";
+
         container.appendChild(force_join);
-        srv.appendChild(container);
+        container.appendChild(invite_link);
+        srv.prepend(container);
     }
 })
+
+if (window.location.hash.startsWith("#rw-invite=")) {
+    const place = window.location.pathname.split("/")[2];
+    const server = window.location.hash.split("=")[1];
+
+    console.log(server,place)
+
+    window.Roblox.GameLauncher.joinGameInstance(place,server);
+}
